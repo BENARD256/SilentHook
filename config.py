dd889fed3f2eb344aef172c56f49d681719adb60
BENARD256 <ab0779672750@gmail.com>
2026-06-06 20:05:31 +0300

    Db Connection defined in .env

--- config.py
+++ config.py
@@ -1,26 +1,30 @@
 import os
 from urllib.parse import quote_plus # handles special Characters
- 
- # Ensuring Proper Encoding of Special Characters in Connection String
-username = quote_plus("dbbd_admin")
-password = quote_plus('admin@1234')
-database_name = quote_plus("dbbd")
-host = quote_plus("127.0.0.1")
+from dotenv import load_dotenv
 
+# Load .env
+load_dotenv()
+
+# Read from env and encode safely
+username = quote_plus(os.getenv("DB_USERNAME", "dbbd_admin"))
+password = quote_plus(os.getenv("DB_PASSWORD", "admin@1234"))
+database_name = quote_plus(os.getenv("DB_NAME", "dbbd"))
+host = quote_plus(os.getenv("DB_HOST", "127.0.0.1"))
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # JWT Config
-    #JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "a9a807b1ab3c90e78dc6dda34bc38d9baa51cbcdcd281e24f8c0d13f62a7361b")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 
-    # Call_Back_Url
-    #CALLBACK_URL = os.environ.get('CALLBACK_URL', 'http://127.0.0.1:5000')
-    #CALLBACK_URL  = "http://192.168.100.10:5000"
+    # CALLBACK_URL
+    CALLBACK_URL = os.environ.get('CALLBACK_URL', 'http://127.0.0.1:5000')
     #CALLBACK_URL = "http://dbbd-61716.portmap.host:61716"
-    CALLBACK_URL = "http://192.168.100.10:5000"
+    #CALLBACK_URL = "http://192.168.100.10:5000"
+    #CALLBACK_URL  = "http://5ae1-41-210-143-103.ngrok-free.app"
+
+    # MSQL DECOY SERVER PORT
     MYSQL_PORT = 3308 # Required for running a Decoy Sql Server
 
 class DevelopmentConfig(Config):
